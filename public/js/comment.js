7a225777f375aa85c7db74ff957de19ce6c24c79
nicholsz <nichols.zane16@gmail.com>
2023-02-27 00:59:09 -0600

    logic for backend done

---
+++ public/js/comment.js
@@ -0,0 +1,28 @@
+const submitCommentHandler = async (event) => {
+    event.preventDefault();
+  
+    const commentText = document.querySelector('.comment-input').value.trim();
+  
+    const postId = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+  
+    if (commentText && postId) {
+      const response = await fetch('/api/comment/create', {
+        method: 'POST',
+        body: JSON.stringify({ commentText, postId }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.reload();
+      } else {
+        alert(response.statusText);
+      }
+    }
+  
+  };
+  
+  const commentButton = document
+    .querySelector('.comment-button')
+    .addEventListener('click', submitCommentHandler);
